e6360daa01617f8624697d5a306d10c7c98dee26
Caio Andres <caioandresgit@outlook.com>
2025-12-08 09:00:10 -0300

    feat(lambda): Verifying megabyte's size in lambda handler.

--- index.py
+++ index.py
@@ -1,9 +1,21 @@
 from log import log
+import boto3
 
-def lambda_handler(event, context):
-    # TODO implement
-    log(msg=event)
-    return {
-        'statusCode': 200,
-        'body': event
-    }
+client = boto3.client("s3")
+
+
+async def lambda_handler(event, context):
+    record = event.Records[0]
+
+    Bucket = record.s3.bucket.name
+    Key = record.s3.object.key
+
+    object_result = await client.get_object(Bucket, Key)
+
+    mega_byte = 1024 * 1024
+
+    if object_result.ContentLength > 1 * mega_byte:
+        log("Object much big.")
+        return "Object much big."
+
+    return "Object's size is OK."
